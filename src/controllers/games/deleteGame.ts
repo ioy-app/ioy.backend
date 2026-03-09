c726602d73634f6bb7595b42ab0a228bedf8b72b
wmgcat <developer@wmgcat.net>
2026-03-10 01:02:52 +0300

    fix

--- src/controllers/games/deleteGame.ts
+++ src/controllers/games/deleteGame.ts
@@ -1,6 +1,7 @@
 import { createCode } from "@/services/codes";
 import { getGameById } from "@/services/games";
 import Request from "@/types/request";
+import AccessError from "@/utils/AccessError";
 import { Response } from "express";
 
 /**
@@ -11,6 +12,8 @@ import { Response } from "express";
 const deleteGame = async(req: Request, res: Response): Promise<void> => {
     try {
         const game = await getGameById(Number(req.params.id));
+        if (req.user_id != game.creater_id)
+            throw new AccessError("deleteGame", "errors.denied");
         const code = await createCode(req.user_id, { type: "delete_game", id: game.id });
         console.log(code);
     }
