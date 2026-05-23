c70194a1e68e11eda166abcd7b250916d3b4bc7c
wmgcat <developer@wmgcat.net>
2026-05-23 13:45:40 +0300

    Fix: Added counter for list games/pictures

--- src/controllers/pictures/getPictures.ts
+++ src/controllers/pictures/getPictures.ts
@@ -13,10 +13,11 @@ const per_page = 10;
 const getPictures = async(req: Request, res: Response): Promise<void> => {
   const offset = Number(req?.query?.offset || 0)
   const query_search: string = req.query.search && String(req.query.search);
+  const count = Number(req?.query?.count || per_page);
 
   // Search on games:
   if (query_search) {
-      const [ ids, total ] = await search("pictures", query_search, offset, per_page);
+      const [ ids, total ] = await search("pictures", query_search, offset, count);
       const items = [];
       for (const id of ids) {
           const data = await getPicture(Number(id));
@@ -26,7 +27,7 @@ const getPictures = async(req: Request, res: Response): Promise<void> => {
       res.status(200).json({
           items,
           offset,
-          limit: per_page,
+          limit: count,
           total
       });
       return;
@@ -35,7 +36,7 @@ const getPictures = async(req: Request, res: Response): Promise<void> => {
   const { hits } = await es.search({
     index: "pictures",
     from: offset,
-    size: per_page,
+    size: count,
     sort: [
       { date_created: { order: "desc" } }
     ]
@@ -50,7 +51,7 @@ const getPictures = async(req: Request, res: Response): Promise<void> => {
   res.status(200).json({
     items: pictures,
     offset,
-    limit: per_page,
+    limit: count,
     total: hits?.total?.value
   });
 }
