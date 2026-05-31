4c284aae48ce996d22f081bad8fe8c41dae75a41
wmgcat <developer@wmgcat.net>
2026-06-01 01:31:26 +0300

    fix

--- src/routes/webhooks.ts
+++ src/routes/webhooks.ts
@@ -2,6 +2,6 @@ import KoFi from "@/webhooks/ko-fi";
 import express from "express";
 
 const Router = express.Router();
-Router.post("/ko-fi", KoFi);
+Router.post("/ko-fi", express.urlencoded({ extended: true }), KoFi);
 
 export default Router;
