ee1478c5b84d1f105bad8d7e45ae2b61f5309512
wmgcat <developer@wmgcat.net>
2026-06-06 22:44:08 +0300

    Add caching

--- src/services/reportsAI/getReportResult.ts
+++ src/services/reportsAI/getReportResult.ts
@@ -1,6 +1,7 @@
 import db from "@/lib/db";
 import getAISql from "./getAISql";
 import logger from "@/lib/logger";
+import redis from "@/lib/redis";
 
 /**
  * Get report result
@@ -10,9 +11,24 @@ import logger from "@/lib/logger";
  * return getReportResult("Get last user info")
 */
 const getReportResult = async (query: string, errors: string[] = []): Promise<any> => {
+  const cache_key = `ai:${query}`;
+  const cache = await redis.readWithLog(cache_key);
+  if (cache) {
+    try {
+      const sql = cache;
+      const result = await db.query(sql);
+      return {
+        result,
+        sql
+      }
+    }
+    catch { await redis.delWithLog(cache_key); }
+  }
+  
   const sql = await getAISql(query, errors);
   try {
     const result = await db.query(sql);
+    redis.writeWithLog(cache_key, sql);
     return {
       result: result?.rows,
       sql
